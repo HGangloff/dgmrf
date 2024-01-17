40af8b1ac3b8b397af3a26c45f8fafd13ee522bd
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2024-01-17 17:02:10 +0100

    first test

---
+++ tests/test_conv_graph_layers_equal.py
@@ -0,0 +1,27 @@
+import pytest
+import jax
+import jax.numpy as jnp
+from dgmrf.models import DGMRF
+from dgmrf.utils import get_adjacency_matrix_lattice
+
+def test_equality():
+    L = 1
+    H = W = 40
+
+    key = jax.random.PRNGKey(0)
+    key, subkey = jax.random.split(key, 2)
+
+    dgmrf_conv = DGMRF(subkey, L, height_width=(H, W), init_params=[jnp.array([4., -1, -1, -1, -1, 0.])])
+
+    dgmrf_graph = DGMRF(
+        subkey,
+        L,
+        A_D=(get_adjacency_matrix_lattice(H, W), 4 * jnp.eye(H * W, H * W)),
+        init_params=[jnp.array([1., -1., 1., 0.])]
+    )
+
+    key, subkey = jax.random.split(key, 2)
+    graph_sample = dgmrf_graph.sample(subkey)
+    conv_sample = dgmrf_conv.sample(subkey)
+
+    assert jnp.allclose(graph_sample, conv_sample)
